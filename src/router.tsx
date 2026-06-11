82ae80ec4f3ba336ed580bd437a6e238c8efe631
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-06-11 13:32:29 +0100

    Create router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { RootLayout } from './components/layout/RootLayout'
+import { SimulationFormPage } from './pages/SimulationFormPage'
+import { SimulationResultsPage } from './pages/SimulationResultsPage'
 
 export const router = createBrowserRouter([
   {
+    element: <RootLayout />,
     children: [
       {
         path: '/',
-        element: <h1>Formulario de Simulação</h1>,
+        element: <SimulationFormPage />,
       },
       {
-        path: '/resultado',
-        element: <h1>Resultado da Simulação</h1>,
+        path: '/resultado/:id',
+        element: <SimulationResultsPage />,
       },
       {
         path: '/historico',
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss],
+plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
