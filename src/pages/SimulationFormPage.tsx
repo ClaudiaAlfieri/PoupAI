842ade8ddfce34cf8de59e9b66593c59e27ec5b2
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-06-12 00:29:13 +0100

    Update SimulationFormPage.tsx

--- src/pages/SimulationFormPage.tsx
+++ src/pages/SimulationFormPage.tsx
@@ -3,7 +3,7 @@ import { SimulationHero } from '@/components/features/Simulation/Hero'
 
 export function SimulationFormPage() {
   return (
-    <main className="mx-auto max-w-xl px-4 py-10 sm:py-14">
+    <main className="mx-auto max-w-xl px-4 py-6 sm:py-10">
       <SimulationHero />
       <SimulationForm />
     </main>
